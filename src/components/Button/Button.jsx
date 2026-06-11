b854cd69a2bf298fdd3bf78e9e453936b07b6ef0
mary-varf <bma1989@yandex.ru>
2026-06-11 09:55:42 +0100

    header, button implementation

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,13 +3,14 @@ import cls from "./Button.module.css";
 
 const isPrimary = true;
 
-export const Button = ({ onClick, children }) => {
+export const Button = ({ onClick, isActive, isDisabled, children }) => {
   return (
-    <div
-      className={`${cls.btn} ${isPrimary ? cls.primary : ""}`}
+    <button
+      className={`${cls.btn} ${isActive ? cls.active : ""}`}
       onClick={onClick}
+      disabled={isDisabled}
     >
       {children}
-    </div>
+    </button>
   );
 };
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import {} from "react";
+import ReactLogo from "../../assets/react.svg";
+import { Button } from "../Button";
+import cls from "./Header.module.css";
+
+export const Header = () => {
+  return (
+    <header className={cls.header}>
+      <p>
+        <img src={ReactLogo} alt="react logo" />
+        <span>ReactQuiz</span>
+      </p>
+      <div className={cls.headerButtons}>
+        <Button isDisabled>Add</Button>
+        <Button>Login</Button>
+      </div>
+    </header>
+  );
+};
