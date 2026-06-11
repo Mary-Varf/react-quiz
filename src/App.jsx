a790ebed8301109dba9f782fca96049940f77714
mary-varf <bma1989@yandex.ru>
2026-06-11 19:49:22 +0100

    question page and small loader

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { MainLayout } from "./components/MainLayout";
 import { HomePage } from "./pages/HomePage/HomePage";
 import { NotFoundPage } from "./pages/NotFoundPage";
+import { QuestionPage } from "./pages/QuestionPage";
 
 const App = () => {
   return (
@@ -11,7 +12,7 @@ const App = () => {
           <Route path="/" element={<HomePage />} />
           <Route path="/main" element={<div>main</div>} />
           <Route path="/addquestion" element={<div>addquestion</div>} />
-          <Route path="/question/:id" element={<div>question page</div>} />
+          <Route path="/question/:id" element={<QuestionPage />} />
           <Route path="/forbidden" element={<div>forbidden</div>} />
           <Route path="*" element={<NotFoundPage />} />
         </Route>
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -42,7 +42,6 @@ export const HomePage = () => {
     }
     const cards = await response.json();
     setQuestions(cards);
-    console.log(cards);
     return cards;
   });
 
