ea64d2663cea6eafd87c6dfb1c0e502dabe990ea
mary-varf <bma1989@yandex.ru>
2026-06-11 14:03:53 +0100

    sort logic

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useMemo } from "react";
 import cls from "./HomePage.module.css";
 import { API_URL } from "../../constants";
 import { QuestionCardList } from "../../components/QuestionCardList";
@@ -9,6 +9,7 @@ import { SearchInput } from "../../components/SearchInput";
 export const HomePage = () => {
   const [questions, setQuestions] = useState([]);
   const [searchValue, setSearchValue] = useState("");
+  const [sortSelectValue, setSortSelectValue] = useState("");
 
   const [getQuestions, isLoading, error] = useFetch(async (url) => {
     const response = await fetch(`${API_URL}/${url}`);
@@ -22,22 +23,47 @@ export const HomePage = () => {
   });
 
   useEffect(() => {
-    getQuestions("react");
-  }, []);
+    getQuestions(`react?_sort=${sortSelectValue}`);
+  }, [sortSelectValue]);
 
   const onSearchChangeHandler = (e) => {
     setSearchValue(e.target.value);
   };
 
+  const onSortSelectChangeHandler = (e) => {
+    setSortSelectValue(e.target.value);
+  };
+  const cards = useMemo(
+    () =>
+      questions.filter((d) =>
+        d.question.toLowerCase().includes(searchValue.trim().toLowerCase()),
+      ),
+    [questions, searchValue],
+  );
   return (
     <>
       <div className={cls.controlsContainer}>
         <SearchInput value={searchValue} onChange={onSearchChangeHandler} />
+        <select
+          name=""
+          id=""
+          value={sortSelectValue}
+          onChange={onSortSelectChangeHandler}
+          className={cls.select}
+        >
+          <option value="">sort by</option>
+          <option value="level">level ASC</option>
+          <option value="-level">level DESC</option>
+          <option value="completed">completed ASC</option>
+          <option value="-completed">completed DESC</option>
+        </select>
       </div>
 
       {isLoading && <Loader />}
       {error && <p>{error}</p>}
-      <QuestionCardList cards={questions} />
+      {cards.length === 0 && <p className={cls.noCards}>No cards...</p>}
+
+      <QuestionCardList cards={cards} />
     </>
   );
 };
