8d1062a314ddc87166aebbb3469649341724dfeb
mary-varf <bma1989@yandex.ru>
2026-06-11 10:17:12 +0100

    homePage, questionCard

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { MainLayout } from "./components/MainLayout";
+import { HomePage } from "./pages/HomePage/HomePage";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<MainLayout />}>
-          <Route path="/" element={<div>home</div>} />
+          <Route path="/" element={<HomePage />} />
           <Route path="/main" element={<div>main</div>} />
           <Route path="/addquestion" element={<div>addquestion</div>} />
           <Route path="/forbidden" element={<div>forbidden</div>} />
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,11 @@
+import {} from "react";
+import cls from "./HomePage.module.css";
+import { QuestionCard } from "../../components/QuestionCard";
+
+export const HomePage = () => {
+  return (
+    <div>
+      <QuestionCard />
+    </div>
+  );
+};
