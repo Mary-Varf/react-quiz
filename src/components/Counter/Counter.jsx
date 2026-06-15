9e0ecca02dcdca00d8ef6521109930a241bf37f7
mary-varf <bma1989@yandex.ru>
2026-06-15 12:57:11 +0100

    server brunch init

--- src/components/Counter/Counter.jsx
+++
@@ -1,8 +0,0 @@
-import { useState } from "react";
-import { Button } from "../Button/Button";
-
-export const Counter = () => {
-  const [count, setCount] = useState(0);
-
-  return <Button onClick={() => setCount(++count)}>count is {count}</Button>;
-};
